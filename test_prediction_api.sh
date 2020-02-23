6019b2cd14ab0c32cff64c8061afdb6b2a67fd99
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2020-02-23 19:52:21 +0800

    Add files via upload

---
+++ test_prediction_api.sh
@@ -0,0 +1,1 @@
+curl --data-binary "@bus_ext_2.jpg" -X POST -H "Content-Type: application/octet-stream" -H "Prediction-Key: b5de19956708483c9de4fca851aa568a" https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/b35dc00f-1a23-4f90-a2f1-c406952ff467/detect/iterations/Bus_Numbers_1/image
