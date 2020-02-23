51dac5ba57cb968f44ac2da1e41c76e426c94154
brandon <tangyuhanbrandon@gmail.com>
2020-02-23 21:26:14 +0800

    Deleted Random Folder

--- BusAPI/test_deployed_api.sh
+++
@@ -1,2 +0,0 @@
-curl --data-binary "@bus_ext_1.jpeg" https://brandonbusapi.azurewebsites.net/api/BusAPI?code=zxQ4wH8qT3UQosqYiof6VGcdrsuS8wsm62fUV6YYBAf6lMsqbNPBQg==
-
--- BusAPI/test_local_api.sh
+++
@@ -1,2 +0,0 @@
-curl --data-binary "@bus_ext_1.jpeg" http://localhost:7071/api/BusAPI
-
